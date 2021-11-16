767b72a1fe709b47d60eeadca658b8cc800f9021
Natalio Junior <natalio.junior18@hotmail.com>
2021-11-16 17:12:59 -0300

    feat: created modal component

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import Modal from "./Modal";
 
 const Card = (props) => {
   if (props.imgB !== undefined) {
@@ -12,7 +13,15 @@ const Card = (props) => {
         <div className="__body">
           <h2 className="__title">{props.title}</h2>
           <p className="__description">{props.description}</p>
-          <button className="__button">Veja Mais</button>
+          <Modal className="__modal" title={props.title} img={props.imgA}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Modal>
         </div>
       </div>
     );
@@ -25,7 +34,15 @@ const Card = (props) => {
         <div className="__body">
           <h2 className="__title">{props.title}</h2>
           <p className="__description">{props.description}</p>
-          <button className="__button">Veja Mais</button>
+          <Modal className="__modal" title={props.title} img={props.imgA}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Modal>
         </div>
       </div>
     );
