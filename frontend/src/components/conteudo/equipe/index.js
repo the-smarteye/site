1f5fb9a4227aa97bc9f7ef019aaeb71b9c1a43de
ThaiMedeiros <thaizamedeirossw@gmail.com>
2021-11-25 18:03:11 -0300

    Adicionando front

---
+++ frontend/src/components/conteudo/equipe/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import "./index.css";
+import Membros from './cardsMembros';
+
+// Imagens dos membros da equipe
+import victoriaIris from "../../../assets/imagens/equipe/VictoriaIris.jpg";
+import leonardoCasillo from "../../../assets/imagens/equipe/LeonardoCasillo.jpg";
+import leonardoFranca from "../../../assets/imagens/equipe/LeonardoFrança.jpg";
+import miltonMendes from "../../../assets/imagens/equipe/MiltonMendes.jpg";
+import rogerioFernandes from "../../../assets/imagens/equipe/RogerioFernandes.jpg";
+
+const Equipe = () => {
+
+  return (
+    <Container>
+      <Row id="linhaUm">
+        <Col xs="12">
+          <h1>Equipe</h1>
+        </Col>
+        <Col xs="12" id="subtituloEquipe">
+          <p>
+            O Smarteye conta com uma equipe com um amplo leque de expertises para suprir as necessidades da proposta, entre elas 
+            inteligencia artificial, IoT , manejo ambiental e desenvolvimento.
+          </p>
+        </Col>
+      </Row>
+
+      <Row id="linhaDois">
+        <Membros 
+          img={victoriaIris}
+          nome="Victória Íris"
+          cargo="Lorem Ipsum"
+        />
+
+        <Membros 
+          img={leonardoCasillo}
+          nome="Leonardo Casillo"
+          cargo="Lorem Ipsum"
+        />
+
+        <Membros 
+          img={leonardoFranca}
+          nome="Leonardo França"
+          cargo="Lorem Ipsum"
+        />
+
+        <Membros 
+          img={miltonMendes}
+          nome="Milton Mendes"
+          cargo="Lorem Ipsum"
+        />
+
+        <Membros 
+          img={rogerioFernandes}
+          nome="Rogério Fernandes"
+          cargo="Lorem Ipsum"
+        />
+      </Row>
+    </Container>
+  );
+}
+
+export default Equipe;
---
+++ frontend/src/components/rodape/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import logoFooter from '../../assets/imagens/logos/logoFooter.png';
+import "./index.css";
+
+const Rodape = () => {
+  return (
+    <div>
+      <footer className="text-center text-white">
+        <div className="container p-4 pb-0">
+          <div className="col-12">
+            <img href="/" src={logoFooter} alt="Logo Rodapé" id="logoRodape" />
+          </div><br />
+
+          <div className="col-12">
+            <p><b>Endereço: </b>
+              Av. Francisco Mota, 572 (Campus Leste da UFERSA) - Prédio da PROEC (Antigo CTARN) - Sala IAGRAM, após a FGD. <br /> 
+              Bairro: Costa e Silva CEP: 59625-900, RN
+            </p>
+            <p><b>Email:</b> contato@smarteye.tec.br</p>          
+          </div>
+
+          <section className="mb-4">
+            <a className="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/the_smarteye/" role="button" target="_blank">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+
+            <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button" target="_blank">
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
+
+            <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button" target="_blank">
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
+          </section>
+        </div>
+
+        <div className="text-center p-3" id="copyright">
+          &copy; 2021 Copyright - SmartEye - Todos os direitos reservados. 
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Rodape;
